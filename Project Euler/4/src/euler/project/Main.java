da472be30524938396cd6fa5c017e7d23fa10ba2
Viсtor Skrypkin <jaller2469@mail.ru>
2020-04-14 16:34:03 +0300

    .

---
+++ Project Euler/4/src/euler/project/Main.java
@@ -0,0 +1,31 @@
+package euler.project;
+
+/*
+Largest palindrome product
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i, j;
+        int b;
+        double c = 0;
+
+        for (i = 100; i <= 999; i++) {
+            for(j = 100; j <= 999; j++) {
+
+                if () {
+
+                    break;
+                }
+
+            }
+        }
+        System.out.println(c);
+    }
+}
